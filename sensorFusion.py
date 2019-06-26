cef71f868f00c3de3b42cce0aa711bf56982a22e
adityanarayanan03 <aditya.narayanan03@gmail.com>
2019-06-26 12:18:47 -0500

    Added filter to sensorFusion file for real-time

--- sensorFusion.py
+++ sensorFusion.py
@@ -47,18 +47,31 @@ calAccEuler = np.matrix([[0],[0],[0]])
 calMagEuler = np.matrix([[0],[0],[0]])
 magPsiValues = []
 
-calGyroYaw = []
-calGyroPitch = []
-calGyroRoll = []
-
 gyroRollCorrection = 0
 gyroPitchCorrection = 0
 gyroYawCorrection = 0
+
+accXCorrection = 0
+accYCorrection = 0
+accZCorrection = 0
 
 #Time-Related Variables:
 dt = .0145 #delta T for integration
 times = [] #x-axis for plotting
 runTime = int(input("Time for trial:   ")) #user desired run-time
+
+#Variables for Filtering
+A = np.matrix([[1,0,0,-dt,0,0],[0,1,0,0,-dt,0],[0,0,1,0,0,-dt],[0,0,0,1,0,0],[0,0,0,0,1,0],[0,0,0,0,0,1]])
+B = np.matrix([[dt,0,0],[0,dt,0],[0,0,dt],[0,0,0],[0,0,0],[0,0,0]])
+C = np.matrix([[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,0,0]])
+P = np.identity(6)
+Q = np.identity(6) * .005
+R = np.matrix([[.1,0,0],[0,.1,0],[0,0,10]])
+state_estimate = np.matrix([[0],[0],[0],[0],[0],[0]])
+filteredData = np.matrix([[0],[0],[0]])
+filteredRoll = []
+filteredPitch = []
+filteredYaw = []
 
 #Creating directories if user wants to save data
 saveIndicator = str(input("Do you want to save data from this run? y/n    "))
@@ -68,6 +81,7 @@ if (saveIndicator== "y" or saveIndicator == "Y"):
 	os.mkdir(fileName+'/rawOutput')
 	os.mkdir(fileName+'/angleEstimations')
 	os.mkdir(fileName+'/comparisons')
+	os.mkdir(fileName+'/comparisons/filteredComparisons')
 	
 #Functions for calculating Euler Angles	
 def accCalcEuler(sensorValues):
@@ -103,15 +117,15 @@ def getData(currentTime):
 	gyro = sensor.readGyro()               #Reads the gyro list? from the sensor
 	mag = sensor.readMagnet()              #Reads the magnetometer list? from the sensor
 	times.append(currentTime)
-	gyroXCorr = -1 * gyro['x'] - gyroRollCorrection
+	gyroXCorr = -1 * (gyro['x'] - gyroRollCorrection)
 	gyroRoll.append(gyroXCorr)
 	gyroYCorr = gyro['y'] - gyroPitchCorrection
 	gyroPitch.append(gyroYCorr)
 	gyroZCorr = -1 * gyro['z'] -gyroYawCorrection
 	gyroYaw.append(gyroZCorr)
-	accelXCorr = -1 * (10 * accel['x'] - .0660602258469)
+	accelXCorr = -10 * (accel['x']) - accXCorrection
 	accX.append(accelXCorr)
-	accelYCorr = 10 * accel['y'] - .0519196988708
+	accelYCorr = 10 * (accel['y']) - accYCorrection
 	accY.append(accelYCorr)
 	accelZCorr = 10 * accel['z'] + (9.8005-9.78941028858)
 	accZ.append(accelZCorr)
@@ -124,6 +138,62 @@ def getData(currentTime):
 	sensorValues = [currentTime, gyroXCorr, gyroYCorr, gyroZCorr, accelXCorr, accelYCorr, accelZCorr, magXCorr, magYCorr, magZCorr]
 	dataSet.append(sensorValues)
 	return sensorValues
+	
+#Function for Kalman Filtering
+def kalmanFilter(row, magCorrectionMean):
+	#tell python to modify global forms
+	global A
+	global B
+	global C
+	global P
+	global Q
+	global R
+	global state_estimate
+	
+	#Collect Gyro Data
+	p = math.radians(row[1])
+	q = math.radians(row[2])
+	r = math.radians(row[3])
+	
+	#Collect Accel Data
+	a_x = row[4]
+	a_y = row[5]
+	a_z = row[6]
+	
+	#Collect Mag Data
+	x_mag = row[7]
+	y_mag = row[8]
+	z_mag = row[9]
+	
+	#calculate Angles from measurements
+	phi_hat_acc = math.atan(a_y/ math.sqrt(a_x**2 + a_z**2))
+	theta_hat_acc = math.atan((a_x) / math.sqrt(a_y**2+a_z**2))
+	
+	phi_hat = state_estimate.item((0,0))
+	theta_hat = state_estimate.item((1,0))
+	psi_hat = state_estimate.item((2,0))
+	
+	psi_hat_mag=math.atan((-1*y_mag*math.cos(phi_hat)+z_mag*math.sin(phi_hat))/(x_mag*math.cos(theta_hat)+y_mag*math.sin(theta_hat)*math.sin(phi_hat)+z_mag*math.sin(theta_hat)*math.cos(phi_hat)))
+	psi_hat_mag = 2 * psi_hat_mag - math.radians(2 * magCorrectionMean)
+	
+	phi_dot = p+math.sin(phi_hat)*math.tan(theta_hat)*q+math.cos(phi_hat)*math.tan(theta_hat)*r
+	theta_dot = math.cos(phi_hat)*q - math.sin(phi_hat)*r
+	psi_dot = math.sin(phi_hat)/math.cos(theta_hat)*q + math.cos(phi_hat)/math.cos(theta_hat)*r
+	delta_angle = np.matrix([[phi_dot],[theta_dot],[psi_dot]])
+	
+	#predict actual attitude
+	state_estimate = A * state_estimate + B * delta_angle
+	P = A*P*np.transpose(A) + Q
+	
+	#Update readings
+	Z = np.matrix([[phi_hat_acc],[theta_hat_acc],[psi_hat_mag]])
+	r = Z - C*state_estimate
+	S = R + C*P*np.transpose(C)
+	K = P*np.transpose(C)*(np.linalg.inv(S)) 
+	state_estimate = state_estimate + K*r
+	P = (np.identity(6) - K*C) * P;
+	state_estimate_degrees = state_estimate * (180/math.pi)
+	return state_estimate_degrees
 	
 #Template function for graphing and saving three-series of data
 def graph(figNum,series1,label1,series2,label2,series3,label3,plotTitle,plotYLabel,savePath):
@@ -149,6 +219,9 @@ def saveAndPlot():
 	graph(7,gyroEulerYaw,'gyroEulerYaw',accEulerYaw,'accEulerYaw',magEulerYaw,'magEulerYaw',"Yaw Angle Comparisons","Angle (degrees)","/comparisons/yawComparisons.png")
 	graph(8,gyroEulerPitch,'gyroEulerPitch',accEulerPitch,'accEulerPitch',magEulerPitch,'magEulerPitch',"Pitch Angle Comparisons","Angle (degrees)","/comparisons/pitchComparisons.png")
 	graph(9,gyroEulerRoll,'gyroEulerRoll',accEulerRoll,'accEulerRoll',magEulerRoll,'gmagEulerRoll',"Roll Angle Comparisons","Angle (degrees)","/comparisons/rollComparisons.png")
+	graph(10,accEulerRoll,'accEulerRoll',filteredRoll,'filteredRoll',gyroRoll,'gyroRoll','Roll Angle Filter Comparison','Angle (degrees)','/comparisons/filteredComparisons/roll.png')
+	graph(11,accEulerPitch,'accEulerPitch',filteredPitch,'filteredPitch',gyroPitch,'gyroPitch','Pitch Angle Filter Comparison','Angle (degrees)','/comparisons/filteredComparisons/pitch.png')
+	graph(12,magEulerYaw,'magEulerYaw',filteredYaw,'filteredYaw',gyroEulerYaw,'gyroEulerYaw','Yaw Angles Filter Comparison','Anlge (degrees)','/comparisons/filteredComparisons/yaw.png')
 	
 #Calibration Sequence
 print 'calibrating'
@@ -156,18 +229,18 @@ for i in range(100):
 	calSensorValues = getData(0)
 	calAccEuler = accCalcEuler(calSensorValues)
 	calMagEuler = magCalcEuler(calSensorValues, calAccEuler)
-	calGyroRoll.append(calSensorValues[1])
-	calGyroPitch.append(calSensorValues[2])
-	calGyroYaw.append(calSensorValues[3])
 	magPsiValues.append(calMagEuler.item((2,0)))
 	time.sleep(.007)
 magCorrectionMean = np.mean(magPsiValues)
-print magCorrectionMean
-gyroRollCorrection = np.mean(calGyroRoll)
-gyroPitchCorrection = np.mean(calGyroPitch)
-gyroYawCorrection = np.mean(calGyroYaw)
+gyroRollCorrection = np.mean(gyroRoll)
+gyroPitchCorrection = np.mean(gyroPitch)
+gyroYawCorrection = np.mean(gyroYaw)
+accXCorrection = np.mean(accX)
+accYCorrection = np.mean(accY)
+
 print 'calibration over, starting in 3 seconds'
 time.sleep(3)
+print 'starting'
 
 #Resetting Variables Common to calibration sequence and data-collection
 times = []
@@ -211,6 +284,12 @@ while (currentTime<runTime):
 	magEulerRoll.append(magEulerAngle.item((0,0)))
 	magEulerPitch.append(magEulerAngle.item((1,0)))
 	magEulerYaw.append(2 * (magEulerAngle.item((2,0)) - magCorrectionMean))
+	
+	filteredData = kalmanFilter(sensorValues, magCorrectionMean)
+	filteredRoll.append(filteredData.item((0,0)))
+	filteredPitch.append(filteredData.item((1,0)))
+	filteredYaw.append(filteredData.item((2,0)))
+	
 	time.sleep(.007)
 
 #Saving plots if requested
@@ -218,4 +297,3 @@ if(saveIndicator == "y" or saveIndicator == "Y"):
 	print "Saving data. This takes forever."
 	saveAndPlot()
 
-print magCorrectionMean
