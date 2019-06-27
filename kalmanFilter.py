6aa612f41b7742ea658a6d4a51baebe7e9e59490
adityanarayanan03 <aditya.narayanan03@gmail.com>
2019-06-27 13:21:43 -0500

    Filtering doesn't work. Probably the bias

--- kalmanFilter.py
+++ kalmanFilter.py
@@ -4,71 +4,79 @@ import matplotlib.pyplot as plt
 import math
 import os
 import csv
+import scipy.io
 
 #Create some global variables
-dt = .0145
+dt = .01
 A = np.matrix([[1,0,0,-dt,0,0],[0,1,0,0,-dt,0],[0,0,1,0,0,-dt],[0,0,0,1,0,0],[0,0,0,0,1,0],[0,0,0,0,0,1]])
 B = np.matrix([[dt,0,0],[0,dt,0],[0,0,dt],[0,0,0],[0,0,0],[0,0,0]])
 C = np.matrix([[1,0,0,0,0,0],[0,1,0,0,0,0],[0,0,1,0,0,0]])
 P = np.identity(6)
 Q = np.identity(6) * .0008
 R = np.matrix([[.1,0,0],[0,.1,0],[0,0,10]])
-state_estimate = np.matrix([[0],[0],[0],[0],[0],[0]])
+state_estimate = np.transpose([[0,0,0,0,0,0]])
 
 phi_angles = []
 theta_angles = []
 psi_angles = []
 
+true_phi = []
+true_theta= []
+true_psi = []
+
 times = []
 
 #get, open, and read the csv file
-fileName = str(input(":              "))
-dataSet = open("/home/pi/MPU9250/"+fileName+"/rawData.csv")
-fullData = csv.reader(dataSet)
+mat = scipy.io.loadmat('IMU_slow.mat')
+gyroReadingsMatrix = np.matrix(mat['gyroReadings'])
+accelReadingsMatrix = np.matrix(mat['accelReadings'])
+magReadingsMatrix = np.matrix(mat['magReadings'])
+trueAngleMatrix = np.matrix(mat['Euler_ang_true'])
 
 #iterate through dataSet
-for row in fullData:
-	#convert all string readings to float values
-	for i in range(0,10):
-		row[i] = float(row[i])
-		
+for i in range(0,1000):
 	#get list of values into individual values
-	currentTime = row[0]
+	currentTime = i
 	times.append(currentTime)
 	
 	#read gyro values
-	p = math.radians(row[1])
-	q = math.radians(row[2])
-	r = math.radians(row[3])
+	p = float(gyroReadingsMatrix.item((i,0)))
+	q = float(gyroReadingsMatrix.item((i,1)))
+	r = float(gyroReadingsMatrix.item((i,2)))
 	
 	#read and normalize acc Values
-	a_x = row[4]
-	a_y = row[5]
-	a_z = row[6]
-	accNorm = 1#math.sqrt(a_x**2 + a_y**2 + a_z**2)
+	a_x = float(accelReadingsMatrix.item((i,0)))
+	a_y = float(accelReadingsMatrix.item((i,1)))
+	a_z = float(accelReadingsMatrix.item((i,2)))
+	accNorm = 1.00#math.sqrt(a_x**2 + a_y**2 + a_z**2)
 	a_x = a_x/accNorm
 	a_y = a_y/accNorm
 	a_z = a_z/accNorm
 	
 	#read and normalize mag values
-	m_x = row[7]
-	m_y = row[8]
-	m_z = row[9]
-	magNorm = 1#math.sqrt(m_x**2 + m_y**2 + m_z**2)
+	m_x = float(magReadingsMatrix.item((i,0)))
+	m_y = float(magReadingsMatrix.item((i,1)))
+	m_z = float(magReadingsMatrix.item((i,2)))
+	magNorm = 1.00#math.sqrt(m_x**2 + m_y**2 + m_z**2)
 	x_mag = m_x/magNorm
 	y_mag = m_y/magNorm
 	z_mag = m_z/magNorm
 	
+	#save true values in right places
+	true_phi.append(trueAngleMatrix.item((i,0)))
+	true_theta.append(trueAngleMatrix.item((i,1)))
+	true_psi.append(trueAngleMatrix.item((i,2)))
+	
+	
 	#Get Angles from raw measurements
-	phi_hat_acc = math.atan(a_y/ math.sqrt(a_x**2 + a_z**2))
-	theta_hat_acc = math.atan((a_x) / math.sqrt(a_y**2+a_z**2))
+	phi_hat_acc = math.atan(a_y/math.sqrt(a_x**2 + a_z**2))
+	theta_hat_acc = math.atan((a_x)/math.sqrt(a_y**2+a_z**2))
 	
 	phi_hat = state_estimate.item((0,0))
 	theta_hat = state_estimate.item((1,0))
 	psi_hat = state_estimate.item((2,0))
 	
 	psi_hat_mag=math.atan((-1*y_mag*math.cos(phi_hat)+z_mag*math.sin(phi_hat))/(x_mag*math.cos(theta_hat)+y_mag*math.sin(theta_hat)*math.sin(phi_hat)+z_mag*math.sin(theta_hat)*math.cos(phi_hat)))
-	psi_hat_mag = 2 * psi_hat_mag - math.radians(-15.1)
 	
 	phi_dot = p+math.sin(phi_hat)*math.tan(theta_hat)*q+math.cos(phi_hat)*math.tan(theta_hat)*r
 	theta_dot = math.cos(phi_hat)*q - math.sin(phi_hat)*r
@@ -85,15 +93,35 @@ for row in fullData:
 	S = R + C*P*np.transpose(C)
 	K = P*np.transpose(C)*(np.linalg.inv(S)) 
 	state_estimate = state_estimate + K*r
-	P = (np.identity(6) - K*C) * P;
+	P = (np.identity(6) - (K*C)) * P
 	state_estimate_degrees = state_estimate * (180/math.pi)
-	phi_angles.append(-1 * state_estimate_degrees.item((0,0)))
-	theta_angles.append(state_estimate_degrees.item((1,0)))
+	phi_angles.append(state_estimate_degrees.item((0,0)))
+	theta_angles.append(-1*state_estimate_degrees.item((1,0)))
 	psi_angles.append(state_estimate_degrees.item((2,0)))	
 	
 plt.figure(1)
-plt.plot(times, psi_angles, label = 'yaw Axis')
-plt.plot(times, theta_angles, label = 'pitch Axis')
-plt.plot(times, phi_angles, label = 'roll Axis')
+plt.plot(times, true_psi, label = 'True Phi')
+plt.plot(times, phi_angles, label = 'Filtered Phi')
 plt.legend(loc = 'upper right')
-plt.savefig('/home/pi/MPU9250/'+fileName+'/angleEstimations/inverseFunction.png')
+plt.title('True phi vs Filtered')
+plt.xlabel('Time (readings)')
+plt.ylabel('Angle (degrees)')
+plt.show()
+
+plt.figure(2)
+plt.plot(times, true_theta, label = 'True Theta')
+plt.plot(times, theta_angles, label = 'Filtered Theta')
+plt.legend(loc = 'upper right')
+plt.title('True theta vs Filtered')
+plt.xlabel('Time (readings)')
+plt.ylabel('Angle (degrees)')
+plt.show()
+
+plt.figure(3)
+plt.plot(times, true_phi, label = 'True Psi')
+plt.plot(times, psi_angles, label = 'Filtered Psi')
+plt.legend(loc = 'upper right')
+plt.title('True psi vs Filtered')
+plt.xlabel('Time (readings)')
+plt.ylabel('Angle (degrees)')
+plt.show()
--- readMatTest.py
+++ readMatTest.py
@@ -1,1 +1,20 @@
+#Imports
 import scipy.io
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Load File
+mat = scipy.io.loadmat('IMU_slow.mat')
+
+#Print file
+#print mat["gyroReadings"]
+gyroReadingsMatrix = np.matrix(mat['gyroReadings'])
+accelReadingsMatrix = np.matrix(mat['accelReadings'])
+magReadingsMatrix = np.matrix(mat['magReadings'])
+trueAngleMatrix = np.matrix(mat['Euler_ang_true'])
+
+gyro = []
+true = []
+times = []
+for i in range(0,1000):
+	print (accelReadingsMatrix.item((i,1)))
